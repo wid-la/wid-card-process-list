5f14cb32875696c89aa2f40c0e44b9fdc2d09181
Aurelien Chaudagne <ouralien@gmail.com>
2015-10-07 15:35:19 -0700

    Improves styling.

--- wid-card-process-list.js
+++ wid-card-process-list.js
@@ -6,7 +6,26 @@
 
     is: 'wid-card-process-list',
 
-    properties: {}
+    behaviors: [Polymer.WidCardStandardBehavior, Polymer.WidCardWeightBehavior, Polymer.WidCardSubscriptionsBehavior],
+
+    properties: {
+      /**
+       * processes
+       * @type {Object}
+       */
+      processes: {
+        type: Object
+      }
+
+    },
+
+    _rowClass: function _rowClass(index) {
+      var odd = '';
+      if (index % 2 === 0) {
+        odd = 'odd';
+      }
+      return odd + ' flex layout horizontal center-center';
+    }
 
   });
 })();
